430a1114b4fecd491e7d15451d621672eb351615
Ethan Mapel <ezmapel@gmail.com>
2020-02-06 20:16:54 -0600

    Add coding for numOfYears

---
+++ index.js
@@ -0,0 +1,13 @@
+const age = 27;
+const name = "Ethan";
+const numOfYears = 0;
+// Create sentence.
+// Concatenation.
+// '.' notation is used to access KEYS within an OBJECT
+// 'log' is a METHOD.
+// We INVOKE the method with ().
+// We pass in an ARGUMENT.
+
+console.log("My name is " + name + ".");
+console.log("I am " + age + " years old.");
+console.log("I have been coding for " + numOfYears " years")
