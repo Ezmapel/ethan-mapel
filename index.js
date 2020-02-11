63ffecf372e548662e4e555417325e7147b1e906
Ethan Mapel <ezmapel@gmail.com>
2020-02-10 19:57:20 -0600

    Start to use arrays

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-const x = 7;
-const y = 10;
+// create an Array using an Array literal
+let arr = [75, 92, 4, 1099];
+console.log("array", arr);
 
-console.log(x !== y);
+// access the 1st item in the Array
+console.log("first", arr[0]);
 
-const me = {
-  name: "Ethan"
-};
+// access the last item in the Array
+console.log("last", arr[3]);
 
-const someone = {
-  name: "Ethan"
-};
+// print the length of the Array
+console.log("length", arr.length);
 
-console.log(me === someone); // false
+// use the length property to access the last item in the Array
+console.log("last via length", arr[arr.length - 1]);
 
-// Will it spit out true or false? Weirdly enough, false
-// Primitives pass by value (it looks at the value)
-// Objects pass by reference (memory addresses, or memory reference...and two objects, even if they have the same contents, are stored in different memory references, and thus do not pass by reference, hence they churn out false)
-
-// BUT if we get down to the primitive level, then it can churn out a true
-
-console.log(me.name === someone.name); // true
-
-// Next time:
-if (x === y) {
-  console.log("x and y are the same");
-} else {
-  console.log("not the same");
+// with for...of, loop over the Array, modify the value and add to a different Array
+for (const stuff of arr) {
+  console.log("for loop", stuff + 10000000);
 }
+
+const arr2 = [];
+
+for (const item of arr) {
+  arr2.unshift(item - 2000);
+}
+console.log("arr", arr, "arr2", arr2);
