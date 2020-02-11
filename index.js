5670ddbdb3860f23f03ce6c6ed3937ec515f8a8a
Ethan Mapel <ezmapel@gmail.com>
2020-02-10 20:51:42 -0600

    Add more objecs and arrays

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-// create an Array using an Array literal
-let arr = [75, 92, 4, 1099];
-console.log("array", arr);
+let obj = {
+  parents: [
+    {
+      name: "Mary",
+      age: 45,
+      likes: ["hiking", "running", "swimming"]
+    },
+    {
+      name: "John",
+      age: 50
+    }
+  ]
+};
 
-// access the 1st item in the Array
-console.log("first", arr[0]);
+// console.log(obj);
 
-// access the last item in the Array
-console.log("last", arr[3]);
-
-// print the length of the Array
-console.log("length", arr.length);
-
-// use the length property to access the last item in the Array
-console.log("last via length", arr[arr.length - 1]);
-
-// with for...of, loop over the Array, modify the value and add to a different Array
-for (const stuff of arr) {
-  console.log("for loop", stuff + 10000000);
+for (const parent of obj.parents) {
+  console.log(parent);
 }
 
-const arr2 = [];
+let key = "name";
 
-for (const item of arr) {
-  arr2.unshift(item - 2000);
+console.log(obj.parents[0][key]);
+
+// You cannot use . notation on an array...must use []
+// You can use either on an object (. or [])
+
+for (const stuffToDo of obj.parents[0].likes) {
+  console.log(stuffToDo);
 }
-console.log("arr", arr, "arr2", arr2);
+
+// MDN javascript hasOwnProperty
