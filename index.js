4755496baace733294f75c7eed410c93cd252f69
Ethan Mapel <ezmapel@gmail.com>
2020-02-11 21:33:45 -0600

    Add last notes from feb 11

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-// loops
-// don't use while loops much anymore - easy to create an accidental endless loop, and it has a variable defined outside of the local scope (outside the while loop)
+// JavaScript and the Browser
+// Chrome is essentially THE browser to use, unless you are using CSS Grid...then Firefox has excellent dev tools
+// LIKELY INTERVIEW QUESTION: "Stepping through a function" means go through function and debug it line by line - use dev tools to create a breakpoint (stop all code after that) and then walk through it
+const kitten = "";
+const cat = () => {
+  console.log(kitten);
+};
 
-// ? TO DO: create a for loop that TOTALS the numbers from 1 to 100
-
-let num = 0;
-
-for (let i = 1; i <= 100; i++) {
-  num += i;
-}
-
-console.log(num);
-// for loop: variable, what you want variable to end up equaling, and your incrementation
-// for (let i = 2; i <= 100; i++) {
-//   console.log(i);
-// }
-
-let n = 0;
-let whileTotal = 0;
-
-while (n <= 100) {
-  whileTotal += n;
-  n++;
-}
-
-console.log(whileTotal);
+cat();
